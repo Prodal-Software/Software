1a4c31401fb3f32302ea7d232c9b228ac7376596
GuilhermeSimaos <guilhermesimaogh@outlook.com>
2023-06-02 23:02:16 -0300

    Added Cadastro de Doação

--- src/src/components/AdmDoação.js
+++ src/src/components/AdmDoação.js
@@ -5,13 +5,24 @@ import {
   Stack,
   TextField,
   MenuItem,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
 } from "@mui/material";
 import Swal from "sweetalert2";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 export const AdmDoação = () => {
   const [doador, setDoador] = useState("");
-  const [endereço, setEndereço] = useState("");
+  const [local, setLocal] = useState("");
+  const [box, setBox] = useState("");
+  const [rua, setRua] = useState("");
+  const [numero, setNumero] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [complemento, setComplemento] = useState("");
   const [alimento, setAlimento] = useState("");
   const [qtd, setQtd] = useState("");
   const [tempo, setTempo] = useState("");
@@ -19,8 +30,26 @@ export const AdmDoação = () => {
   const handleChangeDoador = (Event) => {
     setDoador(Event.target.value);
   };
-  const handleChangeEndereço = (Event) => {
-    setEndereço(Event.target.value);
+  const handleChangeLocal = (Event) => {
+    setLocal(Event.target.value);
+  };
+  const handleChangeBox = (Event) => {
+    setBox(Event.target.value);
+  };
+  const handleChangeRua = (Event) => {
+    setRua(Event.target.value);
+  };
+  const handleChangeNumero = (Event) => {
+    setNumero(Event.target.value);
+  };
+  const handleChangeBairro = (Event) => {
+    setBairro(Event.target.value);
+  };
+  const handleChangeCidade = (Event) => {
+    setCidade(Event.target.value);
+  };
+  const handleChangeComplemento = (Event) => {
+    setComplemento(Event.target.value);
   };
   const handleChangeAlimento = (Event) => {
     setAlimento(Event.target.value);
@@ -33,32 +62,76 @@ export const AdmDoação = () => {
   };
   const handleClearTextFields = () => {
     setDoador("");
-    setEndereço("");
+    setLocal("Externo");
+    setRua("");
+    setNumero("");
+    setBairro("");
+    setCidade("");
+    setComplemento("");
+    setBox("");
     setAlimento("");
     setQtd("");
     setTempo("");
   };
   const handleSubmit = () => {
-    if (doador && endereço && alimento && qtd && tempo !== "") {
-      // Realizar o processamento de enviar os dados
-      // OBS: Adicionar +55 no telefone na hora de enviar
-      let data = {
-        doador,
-        endereço,
-        alimento,
-        qtd,
-        tempo,
-      };
+    try {
+      let data = {};
+      /*
+      A sequencia de condicionais aninhados é para verificar se todos 
+      os campos foram preenchidos e enviar o endereço de acordo com
+      o local selecionado.
+      */
+      if (
+        doador &&
+        local &&
+        ((rua && numero && bairro && cidade) || box) &&
+        alimento &&
+        qtd &&
+        tempo !== ""
+      ) {
+        if (local === "Externo") {
+          data = {
+            doador,
+            endereço:
+              rua +
+              ", " +
+              numero +
+              ", " +
+              bairro +
+              ", " +
+              cidade +
+              ", " +
+              complemento,
+            alimento,
+            qtd,
+            tempo,
+          };
+        } else {
+          data = {
+            doador,
+            endereço: box,
+            alimento,
+            qtd,
+            tempo,
+          };
+        }
+        console.log(data);
 
-      console.log(data);
-
-      Swal.fire({
-        icon: "success",
-        title: "Doação Cadastrada!",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-    } else {
+        Swal.fire({
+          icon: "success",
+          title: "Doação Cadastrada!",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }else{
+        Swal.fire({
+          icon: "error",
+          title: "Dados faltando",
+          text: "Por favor, preencha todos os campos.",
+          confirmButtonText: "OK",
+        });
+      }
+    } catch (error) {
       Swal.fire({
         icon: "error",
         title: "Dados faltando",
@@ -69,13 +142,14 @@ export const AdmDoação = () => {
   };
 
   return (
-    <Box display={"flex"} flexDirection={"column"} mt={"5px"}>
+    <Box display={"flex"} flexDirection={"column"}>
       <Stack spacing={1}>
         <Typography variant="h4" color={"darkorange"}>
           Realizar Doação
         </Typography>
 
         <TextField
+          fullWidth
           color="warning"
           label="Nome do Doador"
           type="name"
@@ -85,16 +159,96 @@ export const AdmDoação = () => {
 
         {/* Inserir 2 radio buttons para endereco externo e mpl 
         Mostrar campos de acordo com a selecao*/}
+        <FormControl>
+          <Stack direction={"row"} spacing={2}>
+            <FormLabel
+              id="radio-buttons-group-label"
+              color="warning"
+              sx={{ alignSelf: "center" }}
+            >
+              Local :
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="radio-buttons-group-label"
+              value={local}
+              onChange={handleChangeLocal}
+            >
+              <FormControlLabel
+                value="Externo"
+                control={<Radio color="warning" />}
+                label="Externo"
+              />
+              <FormControlLabel
+                value="MPL"
+                control={<Radio color="warning" />}
+                label="MPL"
+              />
+            </RadioGroup>
+          </Stack>
+        </FormControl>
 
+        {local === "Externo" && (
+          <Fragment>
+            <Stack direction={"row"} spacing={1}>
+              <TextField
+                fullWidth
+                color="warning"
+                label="Rua"
+                type="text"
+                value={rua}
+                onChange={handleChangeRua}
+              />
+              <TextField
+                sx={{ width: "150px" }}
+                color="warning"
+                label="Numero"
+                type="number"
+                value={numero}
+                onChange={handleChangeNumero}
+              />
+            </Stack>
 
-        {/* !!!Quebrar campos!!! */}
-        <TextField
-          color="warning"
-          label="Endereço"
-          type="text"
-          value={endereço}
-          onChange={handleChangeEndereço}
-        />
+            <Stack direction={"row"} spacing={1}>
+              <TextField
+                color="warning"
+                label="Bairro"
+                type="text"
+                value={bairro}
+                onChange={handleChangeBairro}
+              />
+              <TextField
+                color="warning"
+                label="Cidade"
+                type="text"
+                value={cidade}
+                onChange={handleChangeCidade}
+              />
+            </Stack>
+
+            <TextField
+              multiline
+              color="warning"
+              label="Complemento"
+              type="text"
+              value={complemento}
+              onChange={handleChangeComplemento}
+            />
+          </Fragment>
+        )}
+
+        {local === "MPL" && (
+          <Fragment>
+            <TextField
+              multiline
+              color="warning"
+              label="Box"
+              type="text"
+              value={box}
+              onChange={handleChangeBox}
+            />
+          </Fragment>
+        )}
 
         <Box marginTop={"2%"}>
           <TextField
@@ -111,7 +265,9 @@ export const AdmDoação = () => {
             <MenuItem value={2}>Alface</MenuItem>
             <MenuItem value={3}>Couve</MenuItem>
             <MenuItem value={4}>
-              <Typography sx={{fontStyle:'italic', fontWeight:'bold'}}>Outro</Typography>
+              <Typography sx={{ fontStyle: "italic", fontWeight: "bold" }}>
+                Outro
+              </Typography>
             </MenuItem>
           </TextField>
         </Box>
@@ -161,7 +317,7 @@ export const AdmDoação = () => {
         spacing={5}
         direction={"row"}
         justifyContent={"center"}
-        marginTop={"2em"}
+        marginTop={"10px"}
       >
         <Button
           variant="outlined"
